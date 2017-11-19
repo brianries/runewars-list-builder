ee3c08ee2982b3fcdf80771095364b370b43ebb9
brianries <briantries@gmail.com>
2017-11-19 10:18:33 -0600

    Added a simple unit description panel.  Updated a ton of the CSS settings for padding

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
 import UnitList from './UnitList';
+
+import './App.css';
+
 
 // Needed for onTouchTap (http://stackoverflow.com/a/34015469/988941)
 import injectTapEventPlugin from 'react-tap-event-plugin'
@@ -16,7 +20,18 @@ class App extends Component {
 		return (
 			<MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
 				<AppBar title="Runewars List Builder" /> 		 
-				<UnitList/>    
+				<div className="mainPanel">
+					<div className="unitContainer">
+						<UnitList/>   
+					</div>
+					<div className="descriptionContainer">
+						<Paper className="paper" style={{backgroundColor: 'white', color: 'black', padding: 20}}>
+							Unit Title
+							<br/>
+							Description
+						</Paper>
+					</div>						
+				</div>
 			</MuiThemeProvider>
 		)
 	}
